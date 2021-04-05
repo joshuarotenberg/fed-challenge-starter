9fed2295b82d35d515d6ac2635ff44a70dbe2839
Josh Rotenberg <joshua.rotenberg@gmail.com>
2021-04-05 19:22:51 -0400

    styled overlay and worked in view logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,9 +9,43 @@ const CardContainer = styled.div`
     margin: 0 30px 30px 0;
 `
 
+const ImageWrapper = styled.div`
+   position:relative;
+   display: inline-block;
+`
+const Overlay = styled.div`
+    position: absolute;
+    top:0;
+    right:0;
+    width:115px;
+    height:164px;
+    z-index:9999;
+    background: rgba(0, 0, 0, 0.5);
+    border-top-right-radius: 5px;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+`
+
+const SeriesCount = styled.span`
+    color:white;
+    font-size:24px;
+    font-weight:800;
+`
+const SeriesText = styled.span`
+     color:white;
+    font-size:10px;
+    font-weight:bold;
+    margin-top:5px;
+`
+
 const MainImage = styled.img`
     width: 288px;
     height: 164px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    z-index:99;
 `
 
 const CopyWrapper = styled.div`
@@ -24,7 +58,7 @@ const HeaderWrapper = styled.div`
     display: flex;
     flex-direction:row;
     justify-content: space-between;
-    align-items: center;
+    align-items: start;
 `
 
 const Title = styled.h3`
@@ -72,8 +106,15 @@ const Icon = styled.img`
     background: url("/images/timer-icon.png") no-repeat fixed center;
 `
 
+const SeriesIcon = styled.img`
+    height: 24px;
+    width: 24px;
+    background: url("/images/series-icon.png") no-repeat fixed center;
+    margin-top:10px;
+`
 
-const Card = ({title, image, thumb, duration, distance, type}) => {
+
+const Card = ({title, image, thumb, duration, distance, type, numWorkouts}) => {
     
     let series = false;
     if (type === "collection") {
@@ -82,7 +123,16 @@ const Card = ({title, image, thumb, duration, distance, type}) => {
    
     return (
         <CardContainer>
-            <MainImage src={thumb}></MainImage>
+            {series ? <ImageWrapper>
+                <Overlay>
+                    <SeriesCount>{numWorkouts}</SeriesCount>
+                    <SeriesText>WORKOUTS</SeriesText>
+                    <SeriesIcon src="/images/series-icon.png" alt="series icon"></SeriesIcon>
+                </Overlay>
+                <MainImage src={thumb}></MainImage>
+            </ImageWrapper>
+            : <MainImage src={thumb}></MainImage>
+            }
             <CopyWrapper>
                 <HeaderWrapper>
                     <Title>{title}</Title>
